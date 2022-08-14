f7de5a87c90957f3d267598ca3248bb02f931888
MaximoDM <rtp.iea2010@gmail.com>
2022-08-14 15:33:21 +0100

    fixed duplicated code on index.js

--- index.js
+++ index.js
@@ -13,34 +13,4 @@ app.get('/src/css', (req, res) => {
 
 app.listen(process.env.PORT || 3000, () => console.log('Running.'))
 
-app.use(express.static(__dirname + '/src')); const express = require("express");
-const app = express();
-const path = require('path');
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/index.html'));
-})
-
-app.get('/src/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/index.css'));
-})
-
-
-app.listen(process.env.PORT || 3000, () => console.log('Running.'))
-
-app.use(express.static(__dirname + '/src')); const express = require("express");
-const app = express();
-const path = require('path');
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/index.html'));
-})
-
-app.get('/src/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/styles.css'));
-})
-
-
-app.listen(process.env.PORT || 3000, () => console.log('Running.'))
-
 app.use(express.static(__dirname + '/src'));
